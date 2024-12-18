36e4854e9451507a8bb0be23980fe688f31d1b68
YvYi666 <108977729+YvYi666@users.noreply.github.com>
2024-12-18 15:17:56 +0800

    Update 明文源码.js
    修改proxyIP

--- 明文源码.js
+++ 明文源码.js
@@ -2,7 +2,7 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '32b74825-f0be-4e59-9df3-7e1681d46c38';
-let proxyIP = 'proxyip.jp.fxxk.dedyn.io ';
+let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
